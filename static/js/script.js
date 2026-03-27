31f0d18e8272106966b7f43a6db2ef5425944f1c
SK-Ariyan <skmdariyan777@gmail.com>
2026-03-28 00:07:51 +0530

    Update script.js
    js fixed

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,15 @@
-//board
+// board
 let board;
 let boardWidth = 360;
 let boardHeight = 640;
 let context;
 
-//bird
+// bird
 let birdWidth = 124;
 let birdHeight = 70;
 let birdX = boardWidth / 8;
 let birdY = boardHeight / 2;
-let birdImg;
+let birdImg = new Image();
 
 let bird = {
     x: birdX,
@@ -18,29 +18,30 @@ let bird = {
     height: birdHeight
 };
 
-// Sounds
-let bgMusic = new Audio("/static/sound/pan1.wav");
+// sounds (FIXED PATHS)
+let bgMusic = new Audio("static/sound/pan1.wav");
 bgMusic.loop = true;
 bgMusic.volume = 0.5;
 
-document.addEventListener("keydown", () => {
-    bgMusic.play();
-});
-
-let hitSound = new Audio("/static/sound/aaag.wav");
+let hitSound = new Audio("static/sound/aaag.wav");
 hitSound.volume = 0.9;
 
-//pipes
+// start music on first key press
+document.addEventListener("keydown", () => {
+    bgMusic.play().catch(() => {});
+});
+
+// pipes
 let pipeArray = [];
 let pipeWidth = 64;
 let pipeHeight = 512;
 let pipeX = boardWidth;
 let pipeY = 0;
 
-let topPipeImg;
-let bottomPipeImg;
+let topPipeImg = new Image();
+let bottomPipeImg = new Image();
 
-//physics
+// physics
 let velocityX = -3;
 let velocityY = 1;
 let gravity = 0.3;
@@ -54,15 +55,10 @@ window.onload = function () {
     board.width = boardWidth;
     context = board.getContext("2d");
 
-    //load images
-    birdImg = new Image();
-    birdImg.src = "/static/images/AmitFace.png";
-
-    topPipeImg = new Image();
-    topPipeImg.src = "/static/images/fire.png";
-
-    bottomPipeImg = new Image();
-    bottomPipeImg.src = "/static/images/fireBottom.png";
+    // load images (FIXED PATHS)
+    birdImg.src = "static/images/AmitFace.png";
+    topPipeImg.src = "static/images/fire.png";
+    bottomPipeImg.src = "static/images/fireBottom.png";
 
     requestAnimationFrame(update);
     setInterval(placePipes, 1500);
@@ -75,10 +71,14 @@ function update() {
 
     context.clearRect(0, 0, board.width, board.height);
 
-    // bird
+    // bird physics
     velocityY += gravity;
     bird.y = Math.max(bird.y + velocityY, 0);
-    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+
+    // draw bird ONLY if loaded
+    if (birdImg.complete) {
+        context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    }
 
     if (bird.y > board.height) {
         gameOver = true;
@@ -88,7 +88,11 @@ function update() {
     for (let i = 0; i < pipeArray.length; i++) {
         let pipe = pipeArray[i];
         pipe.x += velocityX;
-        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+
+        // draw pipes safely
+        if (pipe.img.complete) {
+            context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+        }
 
         if (!pipe.passed && bird.x > pipe.x + pipe.width) {
             score += 0.5;
@@ -100,6 +104,7 @@ function update() {
         }
     }
 
+    // remove old pipes
     while (pipeArray.length > 0 && pipeArray[0].x < -pipeWidth) {
         pipeArray.shift();
     }
@@ -111,9 +116,11 @@ function update() {
 
     if (gameOver) {
         context.fillText("GAME OVER", 5, 90);
+
         bgMusic.pause();
         bgMusic.currentTime = 0;
-        hitSound.play();
+
+        hitSound.play().catch(() => {});
     }
 }
 
@@ -145,7 +152,7 @@ function placePipes() {
 }
 
 function moveBird(e) {
-    if (e.code == "Space" || e.code == "ArrowUp" || e.code == "KeyX") {
+    if (e.code === "Space" || e.code === "ArrowUp" || e.code === "KeyX") {
         velocityY = -6;
 
         if (gameOver) {
